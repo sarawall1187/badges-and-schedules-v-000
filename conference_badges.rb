2941d78cb0cc8ddaeeebc8290a8e1b6391c57d5f
Sara Walker <sarawall1187@gmail.com>
2018-12-12 20:28:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.collect do |name|
+    badge_maker(name)
+ end
+end
+
+def assign_rooms(room)
+  room.each_with_index.collect do |name, index|
+     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(attendees)
+    batch_badge_creator(name)
+    assign_rooms(room).each do |attendee|
+      puts attendee
+  end
+end

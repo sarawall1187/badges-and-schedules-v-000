bfe57c63d104a4561e467e7186efb5d51dbc7036
Sara Walker <sarawall1187@gmail.com>
2018-12-12 17:21:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.each do |name|
+    puts badge_maker(name)
+ end
+end
